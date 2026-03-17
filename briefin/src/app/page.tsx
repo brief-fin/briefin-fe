5c56c1e5d542cf3fa9e0908021d18841c63e75b1
kimsh <skim243@buffalo.edu>
2026-03-17 13:25:22 +0900

    design: banner

--- briefin/src/app/page.tsx
+++ briefin/src/app/page.tsx
@@ -1,23 +1,9 @@
-'use client';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-import { LogoIcon, LogoTextIcon } from '../../public/icon';
+import HomeBanner from '@/components/home/HomeBanner';
 
 export default function Page() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      router.push('/login');
-    }, 3000); // 3초 후에 로그인 페이지로 이동
-
-    return () => clearTimeout(timer);
-  }, [router]);
-
   return (
-    <main className="relative flex h-full w-full flex-col items-center pt-72">
-      <LogoIcon />
-      <LogoTextIcon />
+    <main className="flex w-full flex-col gap-8">
+      <HomeBanner />
     </main>
   );
 }
